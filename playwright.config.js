2bee66d46e5f144f061556222b3064c9d28dd316
root <root@localhost.localdomain>
2026-08-24 08:23:56 +0000

    qa: enforce 1 worker and clean launchOptions in playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -4,6 +4,7 @@ const { fixture } = require('./qa/helpers');
 module.exports = defineConfig({
   testDir: './qa',
   outputDir: 'build/reports/playwright-results',
+  workers: 1,
   reporter: [['html', { outputFolder: 'build/reports/playwright-html', open: 'never' }], ['list']],
   use: {
     baseURL: 'http://127.0.0.1:4173',
@@ -12,6 +13,9 @@ module.exports = defineConfig({
     storageState: {
       cookies: [],
       origins: [{ origin: 'http://127.0.0.1:4173', localStorage: [{ name: 'sfp_final_fallback', value: JSON.stringify(fixture()) }] }]
+    },
+    launchOptions: {
+      args: ['--no-sandbox', '--no-zygote', '--disable-gpu', '--disable-dev-shm-usage', '--disable-breakpad', '--no-crashpad']
     }
   },
   webServer: {
